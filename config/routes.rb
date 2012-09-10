fe650b198a56f285eaa960ca090601a4385dd347
Michael Klem <michael.klem@gmail.com>
2012-09-10 16:31:10 -0700

    Clean up around messages

--- config/routes.rb
+++ config/routes.rb
@@ -26,8 +26,6 @@ FirstApp::Application.routes.draw do
   resources :conversations, only: [:index, :show, :new, :create] do
     member do
       post :reply
-      post :trash
-      post :untrash
     end
   end
   
