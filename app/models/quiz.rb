b2c3c70b31f8181aa3ffe6cf987a0501dc5cc30b
marnold1 <mr.arnold@gmail.com>
2012-09-11 11:35:56 -0700

    Fix a few issues with the current menu item not being set. Passing Score now a % On the modules index page admin can preview videos. When playing a course a removed the summary link.

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -3,13 +3,8 @@ class Quiz < ActiveRecord::Base
   
   validates :passing_score, presence: true
   validates_numericality_of :passing_score, :only_integer => true, :message => "is not a number"
-  validate :passing_score_less_than_total_questions
- 
-  def passing_score_less_than_total_questions 
-    if passing_score && self.quiz_questions.size < passing_score
-      self.errors.add(:passing_score, "Passing score must be less than or equal to the number of questions.")
-    end
-  end
+  validates :passing_score, :numericality => { :greater_than => 0, :less_than_or_equal_to => 100 }
+
  
   belongs_to :course_module
   
