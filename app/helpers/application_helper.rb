85c899720f628e8eef93ea8e406ec93a3650fcfe
Michael Klem <michael.klem@gmail.com>
2012-09-10 22:23:31 -0700

    Added helper to render course sub nav menu correctly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,7 +17,25 @@ module ApplicationHelper
   def alert
     flash[:alert]
   end
+
+  def set_admin_mode(value)
+    session[:course_admin_mode] = value
+  end
   
+  def admin_mode?
+    session[:course_admin_mode] == true
+  end
+  
+  def render_course_subnav(menu_class)
+    if admin_mode?
+      render 'courses/courses_admin_menu', :highlight => menu_class
+    else
+      render 'courses/courses_menu', :highlight => menu_class
+    end 
+  end
+  
+  # if the selected_menu class name matches the menu_class
+  # we make that menu item active
   def activate_menu(menu_class, selected_menu)
     class_list = menu_class
     class_list += (selected_menu == menu_class) ? " active" : ""
